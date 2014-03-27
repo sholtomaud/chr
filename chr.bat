5c65e4f702d60f45ae818152c0af570bb7da79eb
sholto maud <sholto.maud@gmail.com>
2014-03-27 14:41:28 +1000

    updating from chr itself

--- chr.bat
+++ chr.bat
@@ -1,6 +1,6 @@
 @rem = ' Perl Script
 @echo off
-set mycd=%cd:\=\\%
+set mycd=homedir_%cd:\=\\%
 perl -wS %0 %1 %2 %mycd%
 goto :EOF
 ';
@@ -14,10 +14,11 @@ use local::lib '';
 use Env;
 use Env qw(PATH HOME TERM);
 use Pod::Select;
+use Cwd;
 
 #Chromicon modules
 use FindBin qw($Bin);
-use lib "$Bin/lib/"; 
+#use lib "$Bin/lib/"; 
 use DevLog;
 
 main: {
@@ -41,10 +42,15 @@ my $dir = 'C:\\Dev\\Logger\\';
 my $git = Git::Wrapper->new($dir);
 
 #print $fh "git dir [".$git->dir."]\n"; 
-
-  #foreach my $arg ( @ARGV ){
-  #  print "arg [$arg]\n";
-  #}
+  my $homedir;
+  foreach my $arg ( @ARGV ){
+    if ( $arg =~ m{^homedir_} ) {
+    
+      my @h = split(/_/, $arg);
+      $homedir = $h[1];
+    }  
+    
+  }
 
   my $statuses = $git->status;
   for my $type (qw<indexed changed unknown conflict>) {
@@ -68,6 +74,14 @@ my $git = Git::Wrapper->new($dir);
   my $keyword = 'IMPTEST';
 
   my %functions = ( start=>1, new=>1, list=>1, 'git.update'=>1, makepod =>1 ) ;
+    my $devlog = DevLog->new( 
+      comment => $comment,
+      status  => $status,
+      keyword => $keyword,
+      script  => $homedir,
+      errmsg  => $errmsg,
+    );
+
   
   if ( lc($ARGV[0]) eq '-help' ){
     print "\nChromicon Utils \n-----------------\n";
@@ -77,19 +91,11 @@ my $git = Git::Wrapper->new($dir);
   }
   elsif ( lc($ARGV[0]) eq 'start'){
     print "ARGV [$ARGV[1]]\n";
-    my $devlog = DevLog->new( 
-      comment => $comment,
-      status  => $status,
-      keyword => $keyword,
-      script  => $ARGV[1],
-      errmsg  => $errmsg,
-      
-    );
+    
     $devlog->dev_log;
   }
   elsif( lc($ARGV[0]) eq 'list'){
-    my $href = DevLog->list(); 
-    
+    my $href = $devlog->list(); 
     my %hash = %$href;
     print "\nProject List\n------------\n";
     foreach my $script ( keys %hash ){
@@ -109,9 +115,60 @@ my $git = Git::Wrapper->new($dir);
     chdir $new_dir;
   }
   elsif ( lc($ARGV[0]) eq 'git.update'){
-    my $href = DevLog->list(); 
-    print "Updating Git Repository\n";
-    
+    my @pm_files = <./lib/*.pm>;
+    print "home dir [$homedir]\n";
+    my $msg = ( $ARGV[1] =~ m{^homedir_} ) ? "Standard commit to correct bugs etc." : $ARGV[1];
+    print "Making POD files\n";
+    foreach my $file ( @pm_files ){ 
+      print "problem with [$file]\n" if ( ! podselect({-output => "README.pod"}, $file) );
+    }
+    print "Running Tests\n";
+    if ( system("dmake test") == 0 ){
+      print "\nUpdating Git Repository\n";
+      my $dir = getcwd;
+      my @dir_components = split(/\//,$dir);
+      my $repo = $dir_components[$#dir_components];
+      my $git = Git::Wrapper->new($dir);
+      my $status = $git->status;
+      
+      print "\n[$repo] repo status\n-------------------\n";
+      for my $type (qw<indexed changed unknown conflict>) {
+        my @states = $status->get($type)
+            or next;
+        print "Files in $type state\n";
+        for (@states) {
+            print '  ', $_->mode, ' ', $_->from;
+            print ' renamed to ', $_->to
+                if $_->mode eq 'renamed';
+            print "\n";
+        }
+      }
+      print "Adding all files to [$repo] repo\n";
+      my $add = $git->add(qw / * /, {all=>1});
+      print "Add returned [$add]\n";
+      my @message = ('--message', qq{$msg});
+      print "Commiting. -m [$msg]\n";
+      my $result  = $git->commit(@message,{ all => 1});
+      print "git commit ok\n" if ($result ==0);
+      #print $_->message for $git->log;
+      my $git_repo = 'https://github.com/shotlom/'.$repo.'.git';
+      print "Git Repo [$git_repo]\n";
+      my @push = ($git_repo,'master');
+      my $push    = $git->push(@push);
+      print "git push ok\n" if ($push == 0 );
+      my $devlog = DevLog->new( 
+        comment => $msg,
+        status  => 'ok',
+        keyword => 'git.update',
+        script  => $homedir,
+        user    => 'smaud',
+        errmsg  => ''
+      );
+      $devlog->dev_log;
+    }
+    else{
+      print "ERROR!!\nProblem with dmake test.\n    Please run dmake test manually from cmd window.\n"
+    }
   }
   elsif ( lc($ARGV[0]) eq 'makepod'){
     my $pm = $ARGV[2].'\\lib\\'.$ARGV[1].'.pm';
@@ -119,6 +176,17 @@ my $git = Git::Wrapper->new($dir);
     #system( podselect $pm > 'README.pod');
     podselect({-output => "README.pod"}, $pm);
     print "Done\n";
+  }
+  elsif ( lc($ARGV[0]) eq 'test'){
+    my @pm_files = </lib/*.pm>;
+    foreach my $file ( @pm_files ){ 
+      print "Making POD from [$file]\n";
+      podselect({-output => "README.pod"}, $file);
+      print "Done\n";
+    }
+    print "dmake test\n";
+    my $err = system("dmake test");
+    print "Output from dmake [$err]\n";
   }
   else {
     print "command [$ARGV[0]] not recognised. Quitting.\n";
